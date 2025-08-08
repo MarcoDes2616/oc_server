63b4d100cd33a7932929deb3d9c39eb900812f45
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-08 13:25:00 -0400

    update
    middlewares integrations

--- src/routes/singnals.routes.js
+++ src/routes/singnals.routes.js
@@ -1,15 +1,17 @@
 const { getAll, create, getOne, remove, update } = require('../controllers/signals.controller');
 const express = require('express');
+const verifyJWT = require('../middlewares/auth.middleware');
+const isAdmin = require('../middlewares/isAdmin.middleware');
 
 const signalsRouter = express.Router();
 
 signalsRouter.route('')
-    .get(getAll)
-    .post(create);
+    .get(verifyJWT, getAll)
+    .post(verifyJWT, isAdmin, create);
 
 signalsRouter.route('/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, isAdmin, remove)
+    .put(verifyJWT, isAdmin, update);
 
 module.exports = signalsRouter;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,15 +1,17 @@
 const { getAll, create, getOne, remove, update } = require('../controllers/users.controller');
 const express = require('express');
+const verifyJWT = require('../middlewares/auth.middleware');
+const isAdmin = require('../middlewares/isAdmin.middleware');
 
 const usersRouter = express.Router();
 
 usersRouter.route('')
-    .get(getAll)
-    .post(create);
+    .get(verifyJWT, isAdmin, getAll)
+    .post(verifyJWT, isAdmin, create);
 
 usersRouter.route('/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, isAdmin, remove)
+    .put(verifyJWT, isAdmin, update);
 
 module.exports = usersRouter;
