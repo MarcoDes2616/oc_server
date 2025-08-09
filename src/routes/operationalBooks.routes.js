2e1122bd91c03c008c6d46c00b9aedc0ebbcca83
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-09 17:41:27 -0400

    Update operationalBooks.routes.js

--- src/routes/operationalBooks.routes.js
+++ src/routes/operationalBooks.routes.js
@@ -1,15 +1,16 @@
 const { getAll, create, getOne, remove, update } = require('../controllers/operationalBooks.controller');
 const express = require('express');
+const verifyJWT = require('../middlewares/auth.middleware');
 
 const operationalBooksRouter = express.Router();
 
 operationalBooksRouter.route('')
-    .get(getAll)
-    .post(create);
+    .get(verifyJWT, getAll)
+    .post(verifyJWT, create);
 
 operationalBooksRouter.route('/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
 
 module.exports = operationalBooksRouter;
