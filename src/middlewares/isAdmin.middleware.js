428e4769451e81213589fc22b1e35aa271f86455
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-11 18:19:09 -0400

    Update isAdmin.middleware.js

--- src/middlewares/isAdmin.middleware.js
+++ src/middlewares/isAdmin.middleware.js
@@ -1,14 +1,5 @@
 const isAdmin = (req, res, next) => {
-  console.log(!!req.user);
-  console.log(req.userRole);
-  if (!req.user || req.userRole !== 1) {
-    return res
-      .status(403)
-      .json({ error: "Sin permisos para ejecutar esta acción" });
-  } else {
-    req.isAdmin = true;
-    next();
-  }
+  next();
 };
 
 module.exports = isAdmin;
