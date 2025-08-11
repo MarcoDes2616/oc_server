568a7f9ac0dfc589013433e1a112963eae8286bf
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-11 18:17:39 -0400

    no verrification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,55 +3,7 @@ const Users = require("../models/Users");
 require('dotenv').config();
 
 const verifyJWT = async (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
-    const token = authHeader?.split(' ')[1];
-    
-    if (!authHeader?.startsWith('Bearer ') || !token) {
-        return res.status(401).json({ 
-            code: 'TOKEN_REQUIRED', 
-            message: 'Token de autenticación no proporcionado' 
-        });
-    }
-
-    try {
-        const { user: userData, iat } = jwt.verify(token, process.env.TOKEN_SECRET);
-
-        const user = await Users.findOne({
-            where: { id: userData.id, status: true }
-        });
-
-        if (!user || !user.status) {
-            return res.status(401).json({
-                code: 'USER_NOT_FOUND',
-                message: 'Usuario no existe o está inactivo'
-            });
-        }
-
-        // Convertir last_login a timestamp UNIX (segundos)
-        const lastLoginSeconds = Math.floor(new Date(user.last_login).getTime() / 1000);
-        
-        // Comparar con iat del token
-        // if (iat < lastLoginSeconds) {
-        //     return res.status(401).json({
-        //         code: 'SESSION_EXPIRED',
-        //         message: 'Sesión inválida. Por favor inicie sesión nuevamente'
-        //     });
-        // }
-        req.userRole = userData.role_id;
-        req.iat = iat;
-        req.user = userData;
-        req.userId = userData.id;
-        next();
-    } catch (error) {
-        const message = error.name === 'TokenExpiredError' 
-            ? 'Token expirado' 
-            : 'Token inválido';
-            
-        res.status(401).json({ 
-            code: 'INVALID_TOKEN', 
-            message 
-        });
-    }
+  next();
 };
 
 module.exports = verifyJWT;
--- src/middlewares/firebase.middleware.js
+++ src/middlewares/firebase.middleware.js
@@ -2,30 +2,7 @@ const {storage} = require('../utils/firebase');
 const { ref, uploadBytes, getDownloadURL } = require('firebase/storage');
 
 const firebaseFile = async(req, res, next) => {
-    console.log(req.file);
-    
-    if(!req.file){     
-        next()
-        return
-    }
-    try {
-        const imgRef = ref(storage, `post/${Date.now()}-${req.file.originalname}`);
-        const imgUploaded = await uploadBytes(imgRef, req.file.buffer);
-        req.body.image_reference =  imgUploaded.metadata.fullPath
-        next()
-    } catch (error) {
-        next(error)
-    }
-}
-
-const getFirebaseUrl = async(path) => {
-    try {
-        const newRef = ref(storage, path)
-        const url = await getDownloadURL(newRef)
-        return url
-    } catch (error) {
-        return error
-    }
+   next();
 }
 
 module.exports = {firebaseFile, getFirebaseUrl}
